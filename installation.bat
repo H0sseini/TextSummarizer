b134642f6e36161b66146a024f8e7d90d5cfc52d
Mehdi Hossseini <59258888+H0sseini@users.noreply.github.com>
2025-05-29 15:49:41 +0330

    addding automatic installation for windows users

---
+++ installation.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: Check if Python is available
+where python >nul 2>nul || (
+    echo Python is not installed or not in PATH.
+    pause
+    exit /b 1
+)
+
+
+
+:: Creating environment and activating
+echo Creating environment and activating
+python -m venv venv
+venv\Scripts\activate
+:: conda create --name newenv python=3.10
+:: call "C:\Users\user\Anaconda3\Scripts\activate.bat" newenv
+pip install -r requirements.txt
+
+
+
+pause
